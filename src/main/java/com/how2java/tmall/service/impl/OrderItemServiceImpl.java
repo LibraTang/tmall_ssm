729ebfa4bf95bab6ec341b79745d2eb45a31db76
LibraTang <1026960839@qq.com>
2020-02-09 18:40:27 +0800

    order

---
+++ src/main/java/com/how2java/tmall/service/impl/OrderItemServiceImpl.java
@@ -0,0 +1,89 @@
+package com.how2java.tmall.service.impl;
+
+import com.how2java.tmall.mapper.OrderItemMapper;
+import com.how2java.tmall.pojo.Order;
+import com.how2java.tmall.pojo.OrderItem;
+import com.how2java.tmall.pojo.OrderItemExample;
+import com.how2java.tmall.pojo.Product;
+import com.how2java.tmall.service.OrderItemService;
+import com.how2java.tmall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderItemServiceImpl implements OrderItemService {
+    @Autowired
+    OrderItemMapper orderItemMapper;
+    @Autowired
+    ProductService productService;
+
+    @Override
+    public void add(OrderItem orderItem) {
+        orderItemMapper.insert(orderItem);
+    }
+
+    @Override
+    public void delete(int id) {
+        orderItemMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(OrderItem orderItem) {
+        orderItemMapper.updateByPrimaryKeySelective(orderItem);
+    }
+
+    @Override
+    public OrderItem get(int id) {
+        OrderItem oi = orderItemMapper.selectByPrimaryKey(id);
+        setProduct(oi);
+        return oi;
+    }
+
+    @Override
+    public List list() {
+        OrderItemExample example = new OrderItemExample();
+        example.setOrderByClause("id DESC");
+        List<OrderItem> ois = orderItemMapper.selectByExample(example);
+        setProduct(ois);
+        return ois;
+    }
+
+    @Override
+    public void fill(List<Order> os) {
+        for(Order o : os) {
+            fill(o);
+        }
+    }
+
+    @Override
+    public void fill(Order order) {
+        OrderItemExample example = new OrderItemExample();
+        example.createCriteria().andIdEqualTo(order.getId());
+        example.setOrderByClause("id DESC");
+        List<OrderItem> ois = orderItemMapper.selectByExample(example);
+        setProduct(ois);
+
+        double total = 0;
+        int totalNumber = 0;
+        for(OrderItem oi : ois) {
+            total += oi.getNumber() * oi.getProduct().getPromotePrice();
+            totalNumber += oi.getNumber();
+        }
+        order.setTotal(total);
+        order.setTotalNumber(totalNumber);
+        order.setOrderItems(ois);
+    }
+
+    public void setProduct(OrderItem oi) {
+        Product product = productService.get(oi.getPid());
+        oi.setProduct(product);
+    }
+
+    public void setProduct(List<OrderItem> ois) {
+        for(OrderItem oi : ois) {
+            setProduct(oi);
+        }
+    }
+}
